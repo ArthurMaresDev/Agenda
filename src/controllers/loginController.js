717355843499bc15b3fc8e7adf05800316c3d3e8
Arthur Mares <Arthurmaris29@gmail.com>
2025-09-10 17:23:16 -0300

    cadastro de usuarios

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,30 @@
+const Login = require('../models/loginModel');
+
 exports.index =(req,res) => {
-    res.render('login')
+    res.render('login');
 
 }
+
+exports.register = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('/login/index');
+      });
+      return;
+    } 
+    req.flash('success', "cadastro realizado com Sucesso!");
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+  
+    } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+    
