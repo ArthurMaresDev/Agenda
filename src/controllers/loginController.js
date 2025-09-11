0dfb783f628d07a533a741d791688988c4de60c3
Arthur Mares <Arthurmaris29@gmail.com>
2025-09-11 17:47:11 -0300

    sistema de cadastro com verificação e hash de senha

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -19,7 +19,7 @@ exports.register = async function(req, res) {
     } 
     req.flash('success', "cadastro realizado com Sucesso!");
       req.session.save(function() {
-        return res.redirect('back');
+        return res.redirect('/login/index');
       });
   
     } catch(e) {
@@ -27,4 +27,30 @@ exports.register = async function(req, res) {
     return res.render('404');
   }
 };
+
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('/login/index');
+      });
+      return;
+    } 
+    req.flash('success', "Logado com sucesso!");
+      req.session.user = login.user;
+      req.session.save(function() {
+        return res.redirect('/login/index');
+      });
+  
+    } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+    
     
